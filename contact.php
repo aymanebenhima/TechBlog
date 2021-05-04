e248dec4a48cb9fc3ca9fcdf72f272796ba91d83
Aymane Benhima <aymanebenhima@gmail.com>
2021-05-04 14:59:30 +0000

    Make title dynamic

--- contact.php
+++ contact.php
@@ -1,35 +1,12 @@
+<?php $get_title = "Contact us"; ?>
 <?php require_once("./includes/header.php"); ?>
-        <div id="layoutDefault">
-            <div id="layoutDefault_content">
-                <main>
-                    
-                    <nav class="navbar navbar-marketing navbar-expand-lg bg-white navbar-light">
-                        <div class="container">
-                            <a class="navbar-brand text-dark" href="index.php">Bloggy</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><img src="./img/menu.png" style="height:20px;width:25px" /><i data-feather="menu"></i></button>
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav ml-auto mr-lg-5">
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="index.php">Home </a>
-                                    </li>
-                                    <li class="nav-item dropdown no-caret">
-                                        <a class="nav-link" href="contact.php">Contact</a>
-                                    </li>
-                                    <li class="nav-item dropdown no-caret">
-                                        <a class="nav-link" href="about.php">About</a>
-                                    </li>
-                                </ul>
-                                <a class="btn-teal btn rounded-pill px-4 ml-lg-4" href="backend/signin.php">Sign in<i class="fas fa-arrow-right ml-1"></i></a>
-                                <a class="btn-teal btn rounded-pill px-4 ml-lg-4" href="backend/signup.php">Sign up<i class="fas fa-arrow-right ml-1"></i></a>
-                            </div>
-                        </div>
-                    </nav>
 
                     <header class="page-header page-header-dark bg-gradient-primary-to-secondary">
                         <div class="page-header-content pt-10">
                             <div class="container text-center">
                                 <div class="row justify-content-center">
                                     <div class="col-lg-8">
-                                        <h1 class="page-header-title mb-3">Contact Us</h1>
+                                        <h1 class="page-header-title mb-3"><?php echo $get_title; ?></h1>
                                         <p class="page-header-text">We will back to you in a week!</p>
                                     </div>
                                 </div>
--- single.php
+++ single.php
@@ -1,28 +1,6 @@
+<?php $get_title = "You are reading the post "; ?>
 <?php require_once("./includes/header.php"); ?>
-        <div id="layoutDefault">
-            <div id="layoutDefault_content">
-                <main>
-                    
-                    <nav class="navbar navbar-marketing navbar-expand-lg bg-white navbar-light">
-                        <div class="container">
-                            <a class="navbar-brand text-dark" href="index.php">Bloggy</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><img src="img/menu.png" style="height:20px;width:25px" /><i data-feather="menu"></i></button>
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav ml-auto mr-lg-5">
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="index.php">Home </a>
-                                    </li>
-                                    <li class="nav-item dropdown no-caret">
-                                        <a class="nav-link" href="contact.php">Contact</a>
-                                    </li>
-                                    <li class="nav-item dropdown no-caret">
-                                        <a class="nav-link" href="about.php">About</a>
-                                    </li>
-                                </ul>
-                                <a class="btn-teal btn rounded-pill px-4 ml-lg-4" href="backend/signin.php">Sign in<i class="fas fa-arrow-right ml-1"></i></a>
-                                <a class="btn-teal btn rounded-pill px-4 ml-lg-4" href="backend/signup.php">Sign up<i class="fas fa-arrow-right ml-1"></i></a>
-                            </div>
-                        </div>
-                    </nav>
+
 
                     <?php
                         if (isset($_GET['post_id'])) {
@@ -110,31 +88,5 @@
                             <!--end comment section end-->
                         </div>
 
-                        <!--Rounded style-->
-                        <div class="svg-border-rounded text-dark">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0" /></svg>
-                        </div>
-                        <!--Rounded style-->
-                    </section>
-                </main>
-            </div>
-
-            <!--Footer start-->
-            <div id="layoutDefault_footer">
-                <footer class="footer pt-4 pb-4 mt-auto bg-dark footer-dark">
-                    <div class="container">
-                        <hr class="my-1" />
-                        <div class="row align-items-center">
-                            <div class="col-md-6 small">Copyright &#xA9; Your Website 2020</div>
-                            <div class="col-md-6 text-md-right small">
-                                <a href="#">Privacy Policy</a>
-                                &#xB7;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
-            <!--Footer end-->
 
 <?php require_once("./includes/footer.php"); ?>
