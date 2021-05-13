2b19fe6f471dcb9e482b242320b87a8900964a90
Aymane Benhima <aymanebenhima@gmail.com>
2021-05-13 00:48:26 +0000

    add signout to account

---
+++ signout.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+    ob_start();
+
+    if ($_SESSION['login']) {
+        session_destroy();
+        unset($_SESSION['login']);
+        unset($_SESSION['user_name']);
+        unset($_SESSION['user_role']);
+        header("Location: index.php");
+    }
